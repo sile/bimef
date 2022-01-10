d50b092718f0bfc62ef8031632d0f995cf458c55
Takeru Ohta <phjgt308@gmail.com>
2022-01-10 20:52:32 +0900

    Implement modified Cholesky decomposition

--- examples/enhance.rs
+++ examples/enhance.rs
@@ -3,7 +3,7 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    #[structopt(default_value = "examples/test.png")]
+    #[structopt(default_value = "examples/test-small.png")]
     image_path: PathBuf,
     #[structopt(default_value = "examples/enhanced.png")]
     output_path: PathBuf,
