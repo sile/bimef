3bdab53a9a5beeae721cc5a3be588918929d6c0f
Takeru Ohta <phjgt308@gmail.com>
2022-01-12 18:25:33 +0900

    Remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
-use pcgs::sparse_symmetric_matrix::{Entry, SparseSymmetricMatrix};
 use std::collections::BTreeMap;
 
 #[derive(Debug)]
 pub struct Bimef {
     /// Enhancement ratio.
     mu: f64,
-
-    /// Exposure ratio.
-    k: Option<Vec<f32>>,
 
     /// Camera response model parameter.
     _a: f64,
@@ -20,32 +16,20 @@ impl Bimef {
     pub fn new() -> Self {
         Self {
             mu: 0.5,
-            k: None,
             _a: -0.3293,
             _b: 1.1258,
         }
     }
 
     pub fn enhance(&self, image: Image<Rgb<f64>>) -> Image<Rgb<u8>> {
-        let lamb = 0.5;
-        let sigma = 5.0;
+        let start = std::time::Instant::now();
 
         let im = IlluminationMap::new(&image);
-
-        // TODO: resize 0.5 => tsmooth => resize 2
-        let t_our = self.tsmooth(&im, lamb, sigma);
-
-        let j = if self.k.is_none() {
-            dbg!(t_our.iter().take(10).collect::<Vec<_>>());
-
-            let is_bad = t_our.iter().map(|&v| v < 0.5).collect::<Vec<_>>();
-            let bads = is_bad.iter().copied().filter(|&b| b).count();
-            let goods = is_bad.len() - bads;
-            dbg!((goods, bads));
-            self.max_entropy_enhance(&image, &is_bad)
-        } else {
-            todo!()
-        };
+        let t_our = im.iter_f2().collect::<Vec<_>>();
+        let is_bad = t_our.iter().map(|&v| v < 0.5).collect::<Vec<_>>();
+        let bads = is_bad.iter().copied().filter(|&b| b).count();
+        let goods = is_bad.len() - bads;
+        let j = self.max_entropy_enhance(&image, &is_bad);
 
         // Weight matrix.
         let image2 = image.map(|i, rgb| {
@@ -76,13 +60,15 @@ impl Bimef {
                 b: to_u8(a.b + b.b),
             }
         });
-
+        println!("Elapsed: {:?}", start.elapsed());
         fused
     }
 
     fn max_entropy_enhance(&self, image: &Image<Rgb<f64>>, is_bad: &[bool]) -> Image<Rgb<f64>> {
         // TODO: resize 50x50
         let y = image.to_gray();
+        let n = 50 * 50;
+        let m = std::cmp::max(1, y.pixels.len() / n);
         let y = y
             .pixels
             .iter()
@@ -90,6 +76,9 @@ impl Bimef {
             .zip(is_bad.iter().copied())
             .filter(|x| x.1)
             .map(|x| x.0)
+            .enumerate()
+            .filter(|x| x.0 % m == 0)
+            .map(|x| x.1)
             .collect::<Vec<_>>();
 
         struct FindNegativeEntropy {
@@ -141,320 +130,6 @@ impl Bimef {
 
         image.apply_k(best_k)
     }
-
-    fn tsmooth(&self, im: &IlluminationMap, lamb: f64, sigma: f64) -> Vec<f64> {
-        let sharpness = 0.001;
-        let (wx, wy) = self.compute_texture_weights(im, sigma, sharpness);
-        self.solve_linear_equation(im, &wx, &wy, lamb)
-    }
-
-    fn solve_linear_equation(
-        &self,
-        im: &IlluminationMap,
-        wx: &IlluminationMap,
-        wy: &IlluminationMap,
-        lamb: f64,
-    ) -> Vec<f64> {
-        let r = im.y_len() as isize;
-        let c = im.x_len() as isize;
-        let k = r * c;
-        let dx = wx.iter_f().map(|v| -lamb * v).collect::<Vec<_>>();
-        let dy = wy.iter_f().map(|v| -lamb * v).collect::<Vec<_>>();
-        let dxa = wx.iter_2x().map(|v| -lamb * v).collect::<Vec<_>>();
-        let dya = wy.iter_2y().map(|v| -lamb * v).collect::<Vec<_>>();
-        let dxd1 = wx.iter_3x().map(|v| -lamb * v).collect::<Vec<_>>();
-        let dyd1 = wy.iter_3y().map(|v| -lamb * v).collect::<Vec<_>>();
-        let dxd2 = wx.iter_4x().map(|v| -lamb * v).collect::<Vec<_>>();
-        let dyd2 = wy.iter_4y().map(|v| -lamb * v).collect::<Vec<_>>();
-
-        let ax = SparseMatrix::from_diags([(dxd1, -k + r), (dxd2, -r)].into_iter(), k as usize);
-        let ay = SparseMatrix::from_diags([(dyd1, -r + 1), (dyd2, -1)].into_iter(), k as usize);
-
-        let mut d = Vec::with_capacity(dx.len());
-        for i in 0..dx.len() {
-            d.push(1.0 - (dx[i] + dy[i] + dxa[i] + dya[i]));
-        }
-
-        let axy = ax.add(ay);
-        // let axy_t = axy.t();
-        // let a = axy.add(axy_t.add(SparseMatrix::from_diag(d)));
-        let a = axy.add(SparseMatrix::from_diag(d));
-
-        let tin = im.iter_f().collect::<Vec<_>>();
-
-        let start = std::time::Instant::now();
-        let m = SparseSymmetricMatrix::new(
-            &a.matrix
-                .iter()
-                .map(|(&(y, x), &v)| e(y, x, v))
-                .collect::<Vec<_>>(),
-        );
-
-        let r = pcgs::solver::solver(&m, &pcgs::vector::Vector(tin));
-        dbg!(r.completed);
-        dbg!(r.iterations);
-        let tout = r.best_guess.0;
-        println!("pcgs: {:?}", start.elapsed());
-
-        // let start = std::time::Instant::now();
-        // let factor = a.cholesky();
-        // println!("Cholesky: {:?}", start.elapsed());
-
-        // let start = std::time::Instant::now();
-        // let tout = factor.solve(&a, &tin);
-        // println!("Solve: {:?}", start.elapsed());
-
-        tout
-    }
-
-    fn compute_texture_weights(
-        &self,
-        im: &IlluminationMap,
-        sigma: f64,
-        sharpness: f64,
-    ) -> (IlluminationMap, IlluminationMap) {
-        let dt0_v = im.diff_v();
-        let dt0_h = im.diff_h();
-
-        let mut gauker_v = dt0_v.convolve_v(sigma as usize);
-        let mut gauker_h = dt0_h.convolve_h(sigma as usize);
-
-        gauker_v.calc_weights(&dt0_v, sharpness);
-        gauker_h.calc_weights(&dt0_h, sharpness);
-        (gauker_h, gauker_v)
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct Factor {
-    d: Vec<f64>,
-    l: BTreeMap<(usize, usize), f64>,
-}
-
-impl Factor {
-    // ICCGSolver
-    pub fn solve(&self, a: &SparseMatrix, b: &[f64]) -> Vec<f64> {
-        let n = self.d.len();
-        let mut x = vec![0.0; n];
-        let mut r = b.to_owned();
-
-        let lu_u = self.lu_u();
-        let mut p = vec![0.0; n];
-        self.icres(&lu_u, &r, &mut p);
-
-        fn dot(a: &[f64], b: &[f64]) -> f64 {
-            assert_eq!(a.len(), b.len());
-            a.iter()
-                .copied()
-                .zip(b.iter().copied())
-                .map(|(a, b)| a * b)
-                .sum::<f64>()
-        }
-
-        let mut r2 = vec![0.0; n];
-        let mut rr0 = dot(&r, &p);
-        let max_iter = 50;
-        let eps = 0.1;
-        let mut y = vec![0.0; n];
-
-        for k in 0..max_iter {
-            // y = AP
-            for i in 0..n {
-                y[i] = a.dot(i, &p);
-            }
-
-            let alpha = rr0 / dot(&p, &y);
-            for i in 0..n {
-                x[i] += alpha * p[i];
-                r[i] -= alpha * y[i];
-            }
-
-            self.icres(&lu_u, &r, &mut r2);
-            let rr1 = dot(&r, &r2);
-
-            let e = rr1.sqrt();
-            dbg!((k, rr1, e));
-            if e < eps {
-                dbg!((k, e));
-                break;
-            }
-
-            let beta = rr1 / rr0;
-            for i in 0..n {
-                p[i] = r2[i] + beta * p[i];
-            }
-
-            rr0 = rr1;
-        }
-
-        x
-    }
-
-    fn icres(&self, lu_u: &BTreeMap<(usize, usize), f64>, r: &[f64], u: &mut [f64]) {
-        let n = self.d.len();
-        let mut y = Vec::with_capacity(n);
-        for i in 0..n {
-            let mut rly = r[i];
-            for (&(_, j), &v) in self.l.range((i, 0)..(i, i)) {
-                rly -= v * y[j];
-            }
-            y.push(rly / self.l[&(i, i)]);
-        }
-
-        for i in (0..n).rev() {
-            let mut lu = 0.0;
-            for (&(_, j), &v) in lu_u.range((i, i + 1)..(i, n)) {
-                lu += v * u[j];
-            }
-            u[i] = y[i] - self.d[i] * lu;
-        }
-    }
-
-    fn lu_u(&self) -> BTreeMap<(usize, usize), f64> {
-        self.l.iter().map(|(&(y, x), &v)| ((x, y), v)).collect()
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct SparseMatrix {
-    size: usize,
-    matrix: BTreeMap<(usize, usize), f64>,
-}
-
-impl SparseMatrix {
-    pub fn dot(&self, y: usize, rhs: &[f64]) -> f64 {
-        self.matrix
-            .range((y, 0)..(y, self.size))
-            .map(|(&(_, x), &v)| v * rhs[x])
-            .sum::<f64>()
-    }
-
-    pub fn print(&self) {
-        println!("[");
-        for y in 0..self.size {
-            print!("[");
-            for x in 0..self.size {
-                print!("{},", self.matrix.get(&(y, x)).copied().unwrap_or_default());
-            }
-            println!("],");
-        }
-        println!("]");
-    }
-
-    pub fn from_diags(diags: impl Iterator<Item = (Vec<f64>, isize)>, size: usize) -> Self {
-        let mut matrix = BTreeMap::new();
-        for (vs, offset) in diags {
-            for (x, v) in vs.iter().copied().enumerate() {
-                let y = x as isize - offset;
-                if y >= 0 && (y as usize) < size {
-                    if v != 0.0 {
-                        matrix.insert((y as usize, x), v);
-                    }
-                } else {
-                    // TODO: break if possible
-                }
-            }
-        }
-        Self { size, matrix }
-    }
-
-    pub fn from_diag(diag: Vec<f64>) -> Self {
-        let size = diag.len();
-        let mut matrix = BTreeMap::new();
-        for (i, v) in diag.into_iter().enumerate() {
-            matrix.insert((i, i), v);
-        }
-        Self { size, matrix }
-    }
-
-    pub fn t(&self) -> Self {
-        Self {
-            size: self.size,
-            matrix: self
-                .matrix
-                .iter()
-                .map(|(&(y, x), &v)| ((x, y), v))
-                .collect(),
-        }
-    }
-
-    // incomplete cholesky decomposition
-    pub fn cholesky(&self) -> Factor {
-        let mut d = Vec::new();
-        let mut l = BTreeMap::<(usize, usize), f64>::new();
-
-        for (&(i, j), &v) in &self.matrix {
-            let mut lld = v;
-            let mut vs_i = l.range((i, 0)..(i, j)).peekable();
-            let mut vs_j = l.range((j, 0)..(j, j)).peekable();
-            while vs_i.peek().is_some() && vs_j.peek().is_some() {
-                let k_i = vs_i.peek().unwrap().0 .1;
-                let k_j = vs_j.peek().unwrap().0 .1;
-                if k_i == k_j {
-                    lld -= vs_i.next().unwrap().1 * vs_j.next().unwrap().1 * d[k_i];
-                } else if k_i < k_j {
-                    vs_i.next();
-                } else {
-                    vs_j.next();
-                }
-            }
-            l.insert((i, j), lld);
-
-            if i == j {
-                d.push(1.0 / l[&(i, i)]);
-            }
-        }
-        assert_eq!(d.len(), self.size);
-
-        Factor { d, l }
-    }
-
-    // pub fn cholesky(&self) -> Factor {
-    //     dbg!(self.matrix.len());
-    //     let mut d = Vec::new();
-    //     let mut l = BTreeMap::new();
-
-    //     for (&(i, j), &v) in &self.matrix {
-    //         if i == j {
-    //             let mut ld = v;
-    //             for (&(_, k), &v) in l.range((i, 0)..(i, i)) {
-    //                 ld -= v * v * d[k];
-    //             }
-    //             d.push(ld);
-    //             l.insert((i, i), 1.0);
-    //         } else {
-    //             let mut lld = v;
-    //             let mut vs_i = l.range((i, 0)..(i, j)).peekable();
-    //             let mut vs_j = l.range((j, 0)..(j, j)).peekable();
-    //             while vs_i.peek().is_some() && vs_j.peek().is_some() {
-    //                 let k_i = vs_i.peek().unwrap().0 .1;
-    //                 let k_j = vs_j.peek().unwrap().0 .1;
-    //                 if k_i == k_j {
-    //                     lld -= vs_i.next().unwrap().1 * vs_j.next().unwrap().1 * d[k_i];
-    //                 } else if k_i < k_j {
-    //                     vs_i.next();
-    //                 } else {
-    //                     vs_j.next();
-    //                 }
-    //             }
-
-    //             l.insert((i, j), 1.0 / d[j] * lld);
-    //         }
-    //     }
-
-    //     Factor { d, l }
-    // }
-
-    pub fn add(mut self, rhs: Self) -> Self {
-        for (k, v) in rhs.matrix.into_iter() {
-            *self.matrix.entry(k).or_default() += v;
-        }
-        self
-    }
-
-    pub fn get(&self, y: usize, x: usize) -> f64 {
-        self.matrix.get(&(y, x)).copied().unwrap_or(0.0)
-    }
 }
 
 #[derive(Debug)]
@@ -483,6 +158,12 @@ impl IlluminationMap {
         let x_len = self.x_len();
         let y_len = self.y_len();
         (0..x_len).flat_map(move |x| (0..y_len).map(move |y| self.values[y][x]))
+    }
+
+    pub fn iter_f2(&self) -> impl '_ + Iterator<Item = f64> {
+        let x_len = self.x_len();
+        let y_len = self.y_len();
+        (0..y_len).flat_map(move |y| (0..x_len).map(move |x| self.values[y][x]))
     }
 
     pub fn iter_2x(&self) -> impl '_ + Iterator<Item = f64> {
@@ -768,59 +449,5 @@ impl Rgb<u8> {
 impl Rgb<f64> {
     pub fn max_value(&self) -> f64 {
         self.r.max(self.g.max(self.b))
-    }
-}
-
-fn e(y: usize, x: usize, v: f64) -> Entry {
-    Entry { y, x, v }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn cholesky_works() {
-        let a = [[4., 0.0, 0.0], [12., 37., 0.0], [-16., -43., 98.]];
-        let mut m = SparseMatrix {
-            size: 3,
-            matrix: Default::default(),
-        };
-        for y in 0..3 {
-            for x in 0..3 {
-                if a[y][x] != 0.0 {
-                    m.matrix.insert((y, x), a[y][x]);
-                }
-            }
-        }
-
-        let f = m.cholesky();
-
-        dbg!(&f.d);
-        dbg!(&f.l);
-        // [L]
-        // 4, 0, 0,
-        // 12, 1, 0,
-        // -16, 5, 9,
-
-        // [d]
-        // 0.25, 1, 0.111111,
-
-        println!("{:?}", f.solve(&m, &[4.0, 13.0, -11.0]));
-
-        let m = SparseSymmetricMatrix::new(&vec![
-            e(0, 0, 4.0),
-            e(1, 0, 12.0),
-            e(2, 0, -16.0),
-            e(1, 1, 37.0),
-            e(2, 1, -43.0),
-            e(2, 2, 98.0),
-        ]);
-
-        let r = pcgs::solver::solver(&m, &pcgs::vector::Vector(vec![4.0, 13.0, -11.0]));
-        dbg!(r.completed);
-        dbg!(r.iterations);
-        dbg!(r.best_guess.0);
-        assert!(false);
     }
 }
